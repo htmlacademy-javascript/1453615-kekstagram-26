88a9f951a93aef3649f073ca9e8b8bbd9158fa3b
aleks-yustas <0f4c82@gmail.com>
2022-07-26 17:37:14 +0300

    Добавляет функции редактирования изображения

--- js/upload-form.js
+++ js/upload-form.js
@@ -1,18 +1,28 @@
 import {toggleModalView, isEscapeKey} from './utils.js';
 import {inputHashtags, inputDescription} from './validate-form.js';
+import {onImageUploadEffectChange} from './effects.js';
+import {ImageScaleSetting, onImageScallerClick} from './scale.js';
 
 const imageUploadFormSelector = '.img-upload__overlay';
-
 const imageUploadInputElement = document.querySelector('.img-upload__input');
 const imageUploadButtomCloseElement = document.querySelector('#upload-cancel');
-
+const imagePreviewElement = document.querySelector('.img-upload__preview img');
+const imageScallerOutputElement = document.querySelector('.scale__control--value');
+const imageScallerElement = document.querySelector('.img-upload__scale');
 const imageEffectsElement = document.querySelector('.img-upload__effects');
+const noneEffectControl = document.querySelector('#effect-none');
+
+imageEffectsElement.addEventListener('change', onImageUploadEffectChange);
+imageScallerElement.addEventListener('click', onImageScallerClick);
 
 const resetImageUploadForm = () => {
   imageUploadInputElement.value = '';
   inputHashtags.value = '';
   inputDescription.value = '';
-  imageEffectsElement.value = 'none';
+  imageScallerOutputElement.value = ImageScaleSetting.INITIAL;
+  imagePreviewElement.style = 'none';
+  imagePreviewElement.removeAttribute('class');
+  noneEffectControl.checked = true;
 };
 
 const onUploadEscapeKeydown = (evt) => {
